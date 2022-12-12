2a63e12f2c4074aaac175d7bea04e90eb61da5fa
unknown <sebaslogo@gmail.com>
2022-12-12 16:17:40 -0600

    Cleaned up motion detection

--- main.py
+++ main.py
@@ -2,53 +2,70 @@ import cv2
 import time
 import motion_detection
 import os
+import sys
+import argparse
 
 PATH = 'images'
 os.chdir(PATH)
 
-print("Initializing webcam...")
-imgCap = cv2.VideoCapture(0)
-print("Webcam initialized!")
+def start_capture(save_images=False, show_webcam=False):
 
-FPS = 0.3
+    print("Initializing webcam...")
+    imgCap = cv2.VideoCapture(0)
+    print("Webcam initialized!")
 
-time_elapsed = 0
-prev = 0
-oldFrame = None
-frameNum = 0
+    FPS = 0.3
 
-while True:
+    time_elapsed = 0
+    prev = 0
+    oldFrame = None
+    frameNum = 0
 
-    time_elapsed = time.time() - prev
+    while True:
 
-    if time_elapsed > 1.0/FPS:
+        time_elapsed = time.time() - prev
 
-        # Capture the video frame
-        ret, frame = imgCap.read()
+        if time_elapsed > 1.0/FPS:
 
-        # cleanup
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # gray 
-        resized_frame = cv2.resize(gray_frame, (256, 256)) # resize
-        prepared_frame = cv2.GaussianBlur(src=resized_frame, ksize=(5,5), sigmaX=0) # denoise
+            # Capture the video frame
+            ret, frame = imgCap.read()
 
-        # detect motion and save image
-        if motion_detection.motion_detector(oldFrame, prepared_frame):
-            cv2.imwrite(f"{frameNum}.jpg", prepared_frame)
-            frameNum += 1
+            # cleanup
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # gray 
+            resized_frame = cv2.resize(gray_frame, (256, 256)) # resize
+            prepared_frame = cv2.GaussianBlur(src=resized_frame, ksize=(5,5), sigmaX=0) # denoise
 
-        oldFrame = prepared_frame
-    
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
+            # detect motion and save image
+            if motion_detection.motion_detector(oldFrame, prepared_frame) and save_images:
+                cv2.imwrite(f"{frameNum}.jpg", prepared_frame)
+                frameNum += 1
+
+            oldFrame = prepared_frame
         
-        # q to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Display the resulting frame
+            if show_webcam: cv2.imshow('frame', frame)
+            
+            # q to exit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        prev = time.time()
+            prev = time.time()
 
+    # After the loop release the cap object
+    imgCap.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
 
-# After the loop release the cap object
-imgCap.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
+def read_cmdline():
+    p=argparse.ArgumentParser()
+    p.add_argument('function')
+    p.add_argument("--save-images",type=bool, choices=[True,False],required=False)
+    p.add_argument("--show-webcam",type=bool, choices=[True,False],required=False)
+    args=p.parse_args()
+    return args 
+
+if __name__ == '__main__':
+    args = read_cmdline()
+    print(args)
+    globals()[args.function](args.save_images, args.show_webcam)
+
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Welcome Kittizen
 
 Domestic surveillance of cat population using OpenCV and Yolonet.
+
+Start spying with the motion detector:
+```
+    python main.py start_capture --show-webcam=True --save-images=True
+```
