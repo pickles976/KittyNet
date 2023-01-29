9510f63605135b7366599a480348f21abe8f8400
unknown <sebaslogo@gmail.com>
2023-01-29 14:12:23 -0600

    ugh

--- server/picam.py
+++ server/picam.py
@@ -8,7 +8,7 @@ import numpy as np
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
 SCALE = 0.7
 FPS = 24.0
-SIZE = (640, 480)
+SIZE = (320, 240)
 
 # create handler for each connection
 async def handler(websocket, path):
