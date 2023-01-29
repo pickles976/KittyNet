01a575144706214f6eaafc1b035d311c66223232
pickles976 <sebaslogo@gmail.com>
2023-01-28 22:04:33 -0600

    testing picam

--- server/picam.py
+++ server/picam.py
@@ -1,50 +1,94 @@
-# echo-server.py
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import cv2
-import time
-import asyncio
-import websockets
+# Web streaming example
+# Source code from the official PiCamera package
+# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
 
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
-SCALE = 0.7
-FPS = 1.0
+import io
+import picamera
+import logging
+import socketserver
+from threading import Condition
+from http import server
 
-print("Initializing webcam...")
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-print("Webcam initialized!")
+PAGE="""\
+<html>
+<head>
+<title>Raspberry Pi - Surveillance Camera</title>
+</head>
+<body>
+<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
+<center><img src="stream.mjpg" width="640" height="480"></center>
+</body>
+</html>
+"""
 
-# create handler for each connection
-async def handler(websocket, path):
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
 
-    time_elapsed = 0
-    prev = 0
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
 
-    while True:
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
 
-        time_elapsed = time.time() - prev
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
 
-        if time_elapsed > 1.0/FPS:
-
-            # Capture the video frame
-            camera.capture(rawCapture, format="bgr")
-            frame = rawCapture.array
-
-            # Resize
-            width = int(frame.shape[1] * SCALE)
-            height = int(frame.shape[0] * SCALE)
-            frame = cv2.resize(frame, (width, height))
-
-            # To bytes
-            frameData = cv2.imencode('.jpg', frame, encode_param)[1].tobytes()
-        
-            await websocket.send(frameData)
-
-            prev = time.time()
-
-print("Starting server thread...")
-start_server = websockets.serve(handler, "", 8000)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
+    output = StreamingOutput()
+    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
+    #camera.rotation = 90
+    camera.start_recording(output, format='mjpeg')
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
